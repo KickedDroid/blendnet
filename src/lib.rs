70e0a8d62b5b7ab40ca8f4f9cc7b72ddcc62bdeb
Kicked Droid <24928676+KickedDroid@users.noreply.github.com>
2024-07-28 18:42:31 -0500

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
+use std::error::Error;
 use std::net::IpAddr;
 use std::path::PathBuf;
+use std::process::Command;
 
 #[derive(Debug, Clone)]
 pub struct RenderNode {
@@ -17,14 +19,13 @@ pub enum NodeStatus {
 }
 
 #[derive(Debug, Clone)]
-pub(crate) struct RenderTask {
-    pub(crate) id: String,
-    pub(crate) blend_file: PathBuf,
-    pub(crate) start_frame: u32,
-    pub(crate) end_frame: u32,
-    pub(crate) render_engine: RenderEngine,
-    pub(crate) threads: u32,
-    pub(crate) status: TaskStatus,
+pub struct RenderTask {
+    pub id: String,
+    pub blend_file: PathBuf,
+    pub start_frame: u32,
+    pub end_frame: u32,
+    pub render_engine: RenderEngine,
+    pub threads: u32,
 }
 
 #[derive(Debug, Clone)]
@@ -54,4 +55,39 @@ pub(crate) struct RenderResult {
     task_id: String,
     frame_number: u32,
     output_file: PathBuf,
+}
+
+pub fn execute_render(task: &RenderTask) -> Result<(), Box<dyn Error>> {
+    let args = construct_blender_task(task);
+    
+    let output = Command::new("blender")
+        .args(&args)
+        .output()?;
+    
+    println!("Render complete. Output: {}", String::from_utf8_lossy(&output.stdout));
+    
+    if !output.status.success() {
+        println!("Error: {}", String::from_utf8_lossy(&output.stderr));
+        return Err("Blender render failed".into());
+    }
+    
+    Ok(())
+}
+
+fn construct_blender_task(task: &RenderTask) -> Vec<String> {
+    vec![
+        "-b".to_string(),
+        task.blend_file.display().to_string(),
+        "-s".to_string(),
+        task.start_frame.to_string(),
+        "-e".to_string(),
+        task.end_frame.to_string(),
+        "-t".to_string(),
+        task.threads.to_string(),
+        "-x".to_string(),
+        "1".to_string(),
+        "-o".to_string(),
+        "src/output".to_string(),
+        "-a".to_string(),
+    ]
 }
--- src/main.rs
+++ src/main.rs
@@ -4,58 +4,17 @@ use std::{error::Error, path::PathBuf, process::Command, str::FromStr};
 
 use lib::*;
 
-fn construct_blender_command(task: &RenderTask) -> Vec<String> {
-    vec![
-        "-b".to_string(),
-        task.blend_file.display().to_string(),
-        "-s".to_string(),
-        task.start_frame.to_string(),
-        "-e".to_string(),
-        task.end_frame.to_string(),
-        "-t".to_string(),
-        task.threads.to_string(),
-        "-x".to_string(),
-        "1".to_string(),
-        "-o".to_string(),
-        "src/output/".to_string(),
-        "-a".to_string(),
-    ]
-}
-
-fn execute_render(task: &RenderTask) -> Result<(), Box<dyn Error>> {
-    let args = construct_blender_command(task);
-    
-    let output = Command::new("blender")
-        .args(&args)
-        .output()?;
-    
-    println!("Render complete. Output: {}", String::from_utf8_lossy(&output.stdout));
-    
-    if !output.status.success() {
-        println!("Error: {}", String::from_utf8_lossy(&output.stderr));
-        return Err("Blender render failed".into());
-    }
-    
-    Ok(())
-}
 
 fn main() {
     let rendertask = RenderTask {
-        id: "Device 1".to_string(),
+        id: "1".to_string(),
         blend_file: PathBuf::from_str("overwerk.blend").unwrap(),
         start_frame: 0,
         end_frame: 1,
         render_engine: RenderEngine::Cycles,
         threads: 1,
-        status: TaskStatus::Queued,
     };
-
-    let args = construct_blender_command(&rendertask);
-    
-    //args.iter().for_each(|i| println!("{}", i));
-
 
     execute_render(&rendertask).unwrap();
     
 }
-
